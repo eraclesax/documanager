c8367c637bf6b7746ff5f12d67339f7b8793e00f
eraclesax <eraclesax@gmail.com>
2025-08-20 19:06:33 +0200

    dev: template defunto

--- docdefunto/urls.py
+++ docdefunto/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('lista', DefuntiListView.as_view(), name='lista'),
-    path('defunto/<int:id>', DefuntoView.as_view(), name='defunto'),
+    path('defunti', DefuntiListView.as_view(), name='lista'),
+    path('defunti/<int:id>', DefuntoView.as_view(), name='defunto'),
     # path('generate-pdf/', views.generate_pdf, name='generate_pdf'),
 ]
