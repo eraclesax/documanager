565f0f3c0cef74c572839bbacd15e3f965c47c79
eraclesax <eraclesax@gmail.com>
2025-08-25 14:59:24 +0200

    Debug ping pong

--- docdefunto/urls.py
+++ docdefunto/urls.py
@@ -7,15 +7,12 @@ from .views import ping
 
 urlpatterns = [
     path("ping/", ping),
+    path('defunti', DefuntiListView.as_view(), name='defunti'),
+    path('defunti/<int:id>', DefuntoView.as_view(), name='defunto'),
+    path('defunti/<int:id>/edit', DefuntoEditView.as_view(), name='defunto_edit'),
+    path('defunti/new', DefuntoEditView.as_view(), name='defunto_new'),
+    path("defunti/<int:pk>/delete/", AnagraficaDefuntoDeleteView.as_view(), name="defunto_delete"),
+    path("defunti/<int:id>/docs/", DefuntoDocsView.as_view(), name="defunto_docs"),
+    path("defunti/<int:def_id>/docs/<int:doc_id>/<str:action>", GetDocView.as_view(), name="get_doc"),
+    # path("defunti/<int:def_id>/docs/<int:doc_id>/", EditDocConfig.as_view(), name="edit_doc_config"),
 ]
-
-# urlpatterns = [
-#     path('defunti', DefuntiListView.as_view(), name='defunti'),
-#     path('defunti/<int:id>', DefuntoView.as_view(), name='defunto'),
-#     path('defunti/<int:id>/edit', DefuntoEditView.as_view(), name='defunto_edit'),
-#     path('defunti/new', DefuntoEditView.as_view(), name='defunto_new'),
-#     path("defunti/<int:pk>/delete/", AnagraficaDefuntoDeleteView.as_view(), name="defunto_delete"),
-#     path("defunti/<int:id>/docs/", DefuntoDocsView.as_view(), name="defunto_docs"),
-#     path("defunti/<int:def_id>/docs/<int:doc_id>/<str:action>", GetDocView.as_view(), name="get_doc"),
-#     # path("defunti/<int:def_id>/docs/<int:doc_id>/", EditDocConfig.as_view(), name="edit_doc_config"),
-# ]
